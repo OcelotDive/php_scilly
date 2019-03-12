6954b3ba70a2dd548d77d2a8125ebf9c44bf2281
ocelotdive <oscar1dlh@hotmail.com>
2019-03-12 11:35:42 +0000

    composer.json

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $locationThree = 'St Martin\'s';
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
  <script src="jquery.parallaxer.js"></script>
- <!--script src="./js/weatherScript.js"></script-->
+ <script src="./js/weatherScript.js"></script>
     
 </body>
 
